31313c2679b583df8c0fd9c9e8e71a63c32074a7
searchivarius <leonid@tiziri.licef>
2013-04-23 16:30:37 -0400

    scripts

--- BuildLangModels.sh
+++ BuildLangModels.sh
@@ -7,15 +7,22 @@ OutDir=MyModels
 if [ ! -d $OutDir ] ; then
     mkdir $OutDir
 fi
+FILE=$OutDir/LM-train-100MW.txt
+if [ ! -f $FILE ] ; then
+    cd $OutDir
+    wget http://www.cs.cmu.edu/~roni/11761/project/LM-train-100MW.txt.gz
+    gunzip LM-train-100MW.txt.gz 
+    cd -
+fi
 
-for n in 2 3 4 5 6 7 ; do 
-    ./BuildNGRAM.sh MyData/LM-postag-100MW.txt $OutDir/LM-postag-1M 10000 $n 
-    if [ "$?" != "0" ] ; then
-        echo "Failure for n = $n"
-    fi
-done
+#for n in 2 3 4 5 6 7 ; do 
+    #./BuildNGRAM.sh MyData/LM-postag-100MW.txt $OutDir/LM-postag-1M 10000 $n 
+    #if [ "$?" != "0" ] ; then
+        #echo "Failure for n = $n"
+    #fi
+#done
 
-for n in 2 3 4 5 6 7 8 9 ; do 
+for n in 2 3 4 5 6 7  ; do 
     ./BuildNGRAM.sh MyData/LM-train-100MW.txt $OutDir/LM-train-100MW 10000 $n
     if [ "$?" != "0" ] ; then
         echo "Failure for n = $n"
---
+++ GenFeatures.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for prefix in development training ; do 
+    ./GenFeatures.pl data/${prefix}Set.dat  data/${prefix}SetLabels.dat $prefix.csv MyModels/LM-train-100MW-N2.binlm W2 0 MyModels/LM-train-100MW-N3.binlm W3 0  MyModels/LM-train-100MW-N4.binlm W4 0 MyModels/LM-train-100MW-N5.binlm W5 0 MyModels/LM-train-100MW-N6.binlm W6 0 MyModels/LM-train-100MW-N7.binlm W7 0  
+done
