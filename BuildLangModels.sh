7c2105de7b69b7f8ce9a1d8f33ac2848cb0b2658
searchivarius <leonid@lemire.me>
2013-04-23 23:08:04 -0400

    1. Script fix 2. New evallm that generate text.

--- BuildLangModels.sh
+++ BuildLangModels.sh
@@ -16,7 +16,7 @@ if [ ! -f $FILE ] ; then
 fi
 
 #for n in 2 3 4 5 6 7 ; do 
-    #./BuildNGRAM.sh MyData/LM-postag-100MW.txt $OutDir/LM-postag-1M 10000 $n 
+    #./BuildNGRAM.sh $OutDir/LM-postag-100MW.txt $OutDir/LM-postag-1M 10000 $n 
     #if [ "$?" != "0" ] ; then
         #echo "Failure for n = $n"
         #exit 1
@@ -24,7 +24,7 @@ fi
 #done
 
 for n in 2 3 4 5 6 7  ; do 
-    ./BuildNGRAM.sh MyData/LM-train-100MW.txt $OutDir/LM-train-100MW 10000 $n
+    ./BuildNGRAM.sh $OutDir/LM-train-100MW.txt $OutDir/LM-train-100MW 10000 $n
     if [ "$?" != "0" ] ; then
         echo "Failure for n = $n"
         exit 1
