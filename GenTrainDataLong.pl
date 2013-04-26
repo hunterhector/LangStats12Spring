c31d3f8178b09fb3430ae6ced96ca819a9dcc274
searchivarius <leonid@tiziri.licef>
2013-04-25 21:24:40 -0400

    new generator

---
+++ GenTrainDataLong.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl -w
+#
+# This script is used to generate additional training/validation data.
+# It mimics input file and generates exactly the same # of fake and real examples.
+# It also tries to make sentences with same lengths as in the example set.
+#
+use Eval;
+
+srand(time());
+
+#
+sub Usage {
+    my $err = shift;
+    print "$err\n";
+    print STDERR "Usage: <Filter Word File> <Example File> <Label File> <# of repeatitions> <Binary Lang Model> <Real Sentence File> <Out File> <Out Label File> <Add Len>\n";
+    exit 1
+}
+
+my $FilterWordFile  = $ARGV[0] or Usage("Specify <Filter Word File>");
+my $ExFile          = $ARGV[1] or Usage("Specify <Example File>");
+my $LabelFile       = $ARGV[2] or Usage("Specify <Label File>");
+my $NumRep          = $ARGV[3] or Usage("Specify <# of repetitions>");
+my $BinLangModel    = $ARGV[4] or Usage("Specify <Binary Lang Model>");
+my $RealSentFile    = $ARGV[5] or Usage("Specify <Real Sentence File>");
+my $OutFile         = $ARGV[6] or Usage("Specify <Out File>");
+my $OutLabelFile    = $ARGV[7] or Usage("Specify <Out Label File>");
+my $AddLen          = $ARGV[8] or Usage("Sepcify <Add Length>");
+
+my $qty = @ARGV;
+
+my $DocSep = '~~~~~';
+my @docs = ReadAll($ExFile, $DocSep);
+# Delete the first elem, which is empty
+shift @docs;
+my @labs = ReadAll($LabelFile, '\n');
+
+my @FilterWords = ReadAll($FilterWordFile, '\n');
+my %FilterHash;
+
+for my $w (@FilterWords) {
+    $FilterHash{$w} = 1;
+}
+
+print "Read docs #", scalar(@docs), " lab #", scalar(@labs), " # of words to keep: ".(keys(%FilterHash))."\n";
+
+
+my @RealSent = ReadAll($RealSentFile, '\n');
+my $RealSentQty = scalar(@RealSent);
+
+$|=1;
+
+open(OD, ">$OutFile") or die("Cannot open output data file for writing!");
+open(OL, ">$OutLabelFile") or die("Cannot open output label file for writing!");
+
+for (my $tt = 0; $tt < $NumRep; ++$tt) {
+print "Repeat. ".($tt+1)." out of $NumRep\n";
+for (my $n = 0; $n < @docs; ++$n) {
+    my $lab = $labs[$n];
+
+    print OL "$lab\n";
+    
+    my @tmps  = split(/\n/m, $docs[$n]);
+    #my $SentQty = @tmps;
+    my $SentQty = @tmps + $AddLen;
+    @tmps  = split(/\s+/m,   $docs[$n]);
+    my $WordQty = @tmps;
+
+    my $txt = "";
+    
+    if (!$lab) {
+    # It's much easier to generate a fake sentence, we have a function for this!
+        $txt = GenSent($BinLangModel, $WordQty);
+    } else {
+    # Harder to get a sequence of real sentences, because we will try to obtain a
+    # 1) sentences going one after another
+    # 2) starting from a random place
+    # 3) having $WordQty words.
+    # Which means that the last sentence may be truncated!
+
+        while (1) {
+            my $start = int(rand() * $RealSentQty);
+            my @ww;
+            for (my $j = $start; $j < $RealSentQty; ++$j) {
+                @tmps  = split(/\s+/m,   $RealSent[$j]);
+                for my $w (@tmps) {
+                    if (exists $FilterHash{$w}) {
+                        push @ww, $w;
+                    } else {
+                        push @ww, "<UNK>";
+                    }
+                }
+                last if (@ww >= $WordQty);
+            }
+            if (@ww >= $WordQty) {
+                $txt = join(" ", @ww);
+                last;
+            }
+        }
+    }
+
+    print "Generated ".($n+1)." out of ".@docs."\n";
+
+    $txt =~ s|</s>|</s>\n|gm;
+    $txt =~ s|^\s+<s>|<s>|gm;
+
+    print OD "$DocSep\n$txt";
+}
+}
+
+close OD or die("cannot close data");
+close OL or die("cannot close labels");
+        
+sub ReadAll {
+    my ($nm, $splitStr) = @_;
+    open FD, "<$nm" or die("Cannot open $nm for reading.");
+
+    my $txt = "";
+
+    while (<FD>) {
+        $txt .= $_;
+    }
+
+    my @res = split(/$splitStr/m, $txt);
+}
+
