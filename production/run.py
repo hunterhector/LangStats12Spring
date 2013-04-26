1a60014777722ceb6ec09902bbcb66d7a4455525
hunterhector <hunterhector@gmail.com>
2013-04-26 17:53:44 -0400

    add production folder, use this as the submission testing envrionment

---
+++ production/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+'''This script is supposed to be the high level script
+that control and run all the other script, it read from
+std in and output to std out, any output by other scripts
+are expected to be written into files'''
+
+
