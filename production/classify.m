ec862ae60246885b8ea3702fbaf4c6cea41fa30a
hunterhector <hunterhector@gmail.com>
2013-04-26 22:20:25 -0400

    --dry-run

--- production/classify.m
+++ production/classify.m
@@ -1,8 +1,9 @@
-data_test_org = importdata('flong.csv')
+data_test_org = importdata('features/flong.csv');
 label = data_test_org.data(:,1);
 data_test = data_test_org.data(:,2:end);
 
 N = size(data_test,1);
 
 for j = 1:N,
-	print 0.5 0.5 0
+	fprintf ('%.7f %.7f 0\n',0.5, 0.5);
+end
