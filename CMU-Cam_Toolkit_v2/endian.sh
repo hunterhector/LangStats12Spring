14531a8b62cf7562ce5d1c049afb304ad29ca409
searchivarius <leonid@tiziri.licef>
2013-04-29 01:57:11 -0400

    Committing a hacked toolkit, just in case.

---
+++ CMU-Cam_Toolkit_v2/endian.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Dumb shell script to report endianness.
+
+echo "  
+  #define BIG_ENDIAN      0
+  #define LITTLE_ENDIAN   1
+
+  int little_endian(void)
+  {
+      short int w = 0x0001;
+      char *byte = (char *) &w;
+      return(byte[0] ? LITTLE_ENDIAN : BIG_ENDIAN);
+  }
+
+int main () {  
+  if(!little_endian()) {
+     printf(\"Big-endian, DO NOT set -DSLM_SWAP_BYTES in Makefile\\n\");
+  } 
+  else {
+     printf(\"Little-endian, set -DSLM_SWAP_BYTES in Makefile\\n\");
+  } 
+  return 0;
+}" > test_endian.c
+gcc test_endian.c -o test_endian
+
+# Can use cc if gcc not available.
+
+./test_endian
+rm -f test_endian test_endian.c
---
+++ CMU-Cam_Toolkit_v2/src/test_endian.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+  #define BIG_ENDIAN      0
+  #define LITTLE_ENDIAN   1
+
+  int little_endian(void)
+  {
+      short int w = 0x0001;
+      char *byte = (char *) &w;
+      return(byte[0] ? LITTLE_ENDIAN : BIG_ENDIAN);
+  }
+
+int main () {  
+  if(little_endian()) {
+     printf("-DSLM_SWAP_BYTES");
+  } 
+
+  return 0;
+}
